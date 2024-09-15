32f7d79c1d7db7d1b2192a4d2b68a43ce113687d
Jude-raganit <juderaganit56@gmail.com>
2024-09-15 12:16:43 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+// convert string to number
+let price = "200";
+price = parseInt(price),number(), +
+console.log(price);
+console.log(typeof price);
+
+// convert number to string
+let distance = 150;
+distance= distance. tostring();
+distance= string();
+console.log(distance);
+console.log(typeof distance);
+
+// convert a string to a decimal
+let weight = "72.5"
+weight = parsefloat(weight);
+console.log(weight);
+console.log(typeof weight);
